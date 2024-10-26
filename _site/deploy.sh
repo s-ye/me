b0c14411ae13d9d842294ac6fcb72f37233b931d
Songyu Ye <songye03@Mac.lan>
2024-10-26 15:40:48 -0400

    update travelmap

--- _site/deploy.sh
+++ _site/deploy.sh
@@ -1,6 +1,15 @@
+#!/bin/bash
+
+# Activate the virtual environment
 source "/Users/songye03/Desktop/me/.venv/bin/activate"
+
+# Run the Python script
 python "/Users/songye03/Desktop/me/_site/travelmap.py"
+
+# Navigate to the project directory
 cd "/Users/songye03/Desktop/me"
+
+# Add changes to Git, commit, and push
 git add .
 git commit -m "update travelmap"
 git push
