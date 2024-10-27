0b3d43d2b741093324c5dfec522b1a3a1177b5f2
Songyu Ye <songye03@Mac.lan>
2024-10-26 22:18:07 -0400

    update travelmap

--- travelmap.py
+++ travelmap.py
@@ -26,45 +26,48 @@ all_locations = {
     
     # North-Central China
     "12. Xi’an, China": [34.3416, 108.9398],
-    "13. Pingyao, China": [37.1987, 112.1784],
-    "14. Datong, China": [40.0768, 113.3001],
-    "15. Mount Wutai, China": [39.0585, 113.5931],
-    "16. Shijiazhuang, China": [38.0428, 114.5149],
+    "13. Luoyang, China": [34.6587, 112.4249],       # Henan, near Xi'an
+    "14. Zhengzhou, China": [34.7466, 113.6254],     # Henan, capital of Henan
+    "15. Kaifeng, China": [34.7972, 114.3075],       # Henan, close to Zhengzhou
+    "16. Anyang, China": [36.0976, 114.3924],
+    "17. Pingyao, China": [37.1987, 112.1784],
+    "18. Datong, China": [40.0768, 113.3001],
+    "19. Mount Wutai, China": [39.0585, 113.5931],
+    "20. Shijiazhuang, China": [38.0428, 114.5149],
     
     # Beijing to Shanghai Route
-    "17. Beijing, China": [39.9042, 116.4074],
-    "18. Qufu, China": [35.5964, 116.9919],
-    "19. Mount Tai, China": [36.2506, 117.1018],
-    "20. Qingdao, China": [36.0671, 120.3826],
-    "21. Nanjing, China": [32.0603, 118.7969],
-    "22. Shanghai, China": [31.2304, 121.4737],
-    "23. Fuzhou, China": [26.0765, 119.2911],
-    "24. Xiamen, China": [24.4798, 118.0894],
+    "21. Beijing, China": [39.9042, 116.4074],
+    "22. Qufu, China": [35.5964, 116.9919],
+    "23. Mount Tai, China": [36.2506, 117.1018],
+    "24. Qingdao, China": [36.0671, 120.3826],
+    "25. Nanjing, China": [32.0603, 118.7969],
+    "26. Shanghai, China": [31.2304, 121.4737],
+    "27. Fuzhou, China": [26.0765, 119.2911],
+    "28. Xiamen, China": [24.4798, 118.0894],
     
     # West China (Xinjiang)
-    "25. Urumqi, China": [43.8256, 87.6168],
-    "26. Turpan, China": [42.9513, 89.1897],
-    "27. Kashgar, China": [39.4704, 75.9898],
+    "29. Urumqi, China": [43.8256, 87.6168],
+    "30. Turpan, China": [42.9513, 89.1897],
+    "31. Kashgar, China": [39.4704, 75.9898],
 
-    "Mount Kailash, Tibet": [31.0685, 81.3104],
-    "Lake Manasarovar, Tibet": [30.6476, 81.4634],
-    "Darchen, Tibet": [30.9650, 81.3189],
-    "Pulan County (Burang), Tibet": [30.2972, 81.2464],
-    "Hilsa, Nepal": [30.0381, 81.1750],
+    "32. Mount Kailash, Tibet": [31.0685, 81.3104],
+    "33. Lake Manasarovar, Tibet": [30.6476, 81.4634],
+    "34. Darchen, Tibet": [30.9650, 81.3189],
+    "35. Pulan County (Burang), Tibet": [30.2972, 81.2464],
+    "36. Hilsa, Nepal": [30.0381, 81.1750],
 
-    "28. Lhasa, China (Tibet)": [29.6520, 91.1721],  # Tibet's capital
-    "29. Shigatse, China (Tibet)": [29.2682, 88.8806],  # Major city near Everest
+    "37. Lhasa, China (Tibet)": [29.6520, 91.1721],  # Tibet's capital
+    "38. Shigatse, China (Tibet)": [29.2682, 88.8806],  # Major city near Everest
     # Add Indian cities after crossing
-    # "30. Leh, India": [34.1526, 77.5771],  # A major town in northern India
-    # "31. Srinagar, India": [34.0837, 74.7973],  # Capital of Jammu and Kashmir
-    # "32. Delhi, India": [28.6139, 77.2090],  # India’s capital
-    "Kathmandu, Nepal": [27.7172, 85.3240],  # Nepal's capital
-    "Lumbini, Nepal": [27.4695, 83.2750],
-    "Varanasi, India": [25.3176, 82.9739],
-    "Agra, India": [27.1751, 78.0421],  # Taj Mahal location
-    "Jaipur, India": [26.9124, 75.7873],
-    "Delhi, India": [28.6139, 77.2090],  # India’s capital
-
+    # "39. Leh, India": [34.1526, 77.5771],  # A major town in northern India
+    # "40. Srinagar, India": [34.0837, 74.7973],  # Capital of Jammu and Kashmir
+    # "41. Delhi, India": [28.6139, 77.2090],  # India’s capital
+    "39. Kathmandu, Nepal": [27.7172, 85.3240],  # Nepal's capital
+    "40. Lumbini, Nepal": [27.4695, 83.2750],
+    "41. Varanasi, India": [25.3176, 82.9739],
+    "42. Agra, India": [27.1751, 78.0421],  # Taj Mahal location
+    "43. Jaipur, India": [26.9124, 75.7873],
+    "44. Delhi, India": [28.6139, 77.2090],  # India’s capital
 }
 
 # Add all locations to the map
@@ -78,4 +81,3 @@ for i in range(len(locations_list) - 1):
 
 # Save map to HTML file
 auto_center_map.save("AutoCentered_Map_Route.html")
-
